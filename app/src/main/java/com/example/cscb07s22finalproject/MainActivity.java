a3d9691edc19cb67add46211ebdb8474fbd6d781
BVenom1 <bvenom169@gmail.com>
2022-07-28 15:46:15 -0400

    initial commit, set up project and connected firebase

---
+++ app/src/main/java/com/example/cscb07s22finalproject/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.cscb07s22finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        ref.child("first").setValue(100);
+        ref.child("second").setValue("harry");
+    }
+}
