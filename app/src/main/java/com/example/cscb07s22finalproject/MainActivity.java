cb743f844c9a66d55f92c7fd7f53df93b4aef321
BVenom1 <bvenom169@gmail.com>
2022-07-31 01:06:00 -0400

    refactoring to DataBase class

--- app/src/main/java/com/example/cscb07s22finalproject/MainActivity.java
+++ app/src/main/java/com/example/cscb07s22finalproject/MainActivity.java
@@ -1,72 +1,50 @@
 package com.example.cscb07s22finalproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.util.Log;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.*;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import com.google.firebase.database.DatabaseReference;
+
 
 public class MainActivity extends AppCompatActivity {
-    DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+    DataBase db = DataBase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = db.getRef();
         ref.child("first").setValue(100);
-        ref.child("third").setValue("harry");
-        ref.child("third").setValue("notharry");
-
-        ref.child("first").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    TextView tv = findViewById(R.id.textView1);
-                    Log.e("demo", "Error getting data", task.getException());
-                }
-                else {
-                    TextView tv = findViewById(R.id.textView1);
-                    tv.setText(task.getResult().getValue().toString());
-                }
+        ref.child("first").get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.e("demo", "Error getting data", task.getException());
+            }
+            else {
+                TextView tv = findViewById(R.id.textView1);
+                tv.setText(task.getResult().getValue().toString());
             }
         });
     }
 
     public void signUpActivity(View view) {
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        EditText editText = findViewById(R.id.editTextTextPersonName);
         String username = editText.getText().toString();
 
-        editText = (EditText) findViewById(R.id.editTextTextPassword);
+        editText = findViewById(R.id.editTextTextPassword);
         String password = editText.getText().toString();
 
-        Pattern pattern = Pattern.compile("\\w+");
-        Matcher matcher_user = pattern.matcher(username);
-        Matcher matcher_pass = pattern.matcher(password);
-
-        if(!(matcher_user.matches()) || !(matcher_pass.matches())){
+        if(db.checkUser(username, password) == DataBase.INCORRECT_FORMAT){
             Toast.makeText(MainActivity.this, "Invalid: username & password must be 1 or more word characters only", Toast.LENGTH_LONG).show();
         }else{
-            ref.child("users").child(username).child("username").setValue(username);
-            ref.child("users").child(username).child("password").setValue(password);
-            ref.child("users").child(username).child("adminFlag").setValue(false);
-
+            db.createUser(username, password);
             Intent intent = new Intent(this, UserHomeActivity.class);
             startActivity(intent);
         }
