62978b876bc6c0bff1ff5ddaf3be222ea05e463a
aidand9 <aidan.delmonte@mail.utoronto.ca>
2022-07-31 17:24:34 -0400

    signup

--- app/src/main/java/com/example/cscb07s22finalproject/DataBase.java
+++ app/src/main/java/com/example/cscb07s22finalproject/DataBase.java
@@ -1,7 +1,12 @@
 package com.example.cscb07s22finalproject;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,6 +19,7 @@ public final class DataBase {
 
     public static final int INCORRECT_FORMAT = -1;
     public static final int DOES_NOT_EXIST = -2;
+    public static final int ALREADY_EXISTS = -4;
     public static final int INCORRECT_PASSWORD = -3;
     public static final int CAN_LOGIN = 0;
 
@@ -48,13 +54,35 @@ public final class DataBase {
         Pattern pattern = Pattern.compile("\\w+");
         Matcher matcher_user = pattern.matcher(username);
         Matcher matcher_pass = pattern.matcher(password);
-
+        final int[] num = new int[1];
         if(!(matcher_user.matches()) || !(matcher_pass.matches()))
             return INCORRECT_FORMAT;   // incorrect format
 
         //TODO: other checks
+        ref.child("users").child("username").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                if (snapshot.exists())
+                {
+                    num[0] = 0;
+                }
+            }
 
-        return DOES_NOT_EXIST;
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        if (num[0]==0)
+        {
+            return ALREADY_EXISTS;
+        }
+        else
+        {
+            return DOES_NOT_EXIST;
+        }
+
     }
 
     public void createUser(String username, String password) {
