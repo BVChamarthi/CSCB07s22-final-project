1334abbec0468e5195b4dd47819219a75a9f4e66
aidand9 <aidan.delmonte@mail.utoronto.ca>
2022-07-31 16:28:19 -0400

    test

--- app/src/main/java/com/example/cscb07s22finalproject/MainActivity.java
+++ app/src/main/java/com/example/cscb07s22finalproject/MainActivity.java
@@ -1,41 +1,62 @@
 package com.example.cscb07s22finalproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.database.DataSnapshot;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
+
 
 public class MainActivity extends AppCompatActivity {
+    DataBase db = DataBase.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
+        DatabaseReference ref = db.getRef();
         ref.child("first").setValue(100);
-        ref.child("second").setValue("harry");
-
-        ref.child("first").get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
-            @Override
-            public void onComplete(@NonNull Task<DataSnapshot> task) {
-                if (!task.isSuccessful()) {
-                    TextView tv = findViewById(R.id.textView1);
-                    Log.e("demo", "Error getting data", task.getException());
-                }
-                else {
-                    TextView tv = findViewById(R.id.textView1);
-                    tv.setText(task.getResult().getValue().toString());
-                }
+        ref.child("first").get().addOnCompleteListener(task -> {
+            if (!task.isSuccessful()) {
+                Log.e("demo", "Error getting data", task.getException());
+            }
+            else {
+                TextView tv = findViewById(R.id.textView1);
+                tv.setText(task.getResult().getValue().toString());
             }
         });
+    }
+
+    public void signUpActivity(View view) {
+        EditText editText = findViewById(R.id.editTextTextPersonName);
+        String username = editText.getText().toString();
+
+        editText = findViewById(R.id.editTextTextPassword);
+        String password = editText.getText().toString();
+
+        if(db.checkUser(username, password) == DataBase.INCORRECT_FORMAT){
+            Toast.makeText(MainActivity.this, "Invalid: username & password must be 1 or more word characters only", Toast.LENGTH_LONG).show();
+        }else{
+            db.createUser(username, password);
+            Intent intent = new Intent(this, UserHomeActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void loginAdminActivity(View view) {
+        Intent intent = new Intent(this, AdminHomeActivity.class);
+        startActivity(intent);
+    }
+
+    public void loginUserActivity(View view) {
+        Intent intent = new Intent(this, UserHomeActivity.class);
+        startActivity(intent);
     }
 }
