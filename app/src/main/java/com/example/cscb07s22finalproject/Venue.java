43f79532cac449bed94845c5036e48aaf6041eea
aidand9 <aidan.delmonte@mail.utoronto.ca>
2022-08-01 17:41:03 -0400

    added event and venue classes

---
+++ app/src/main/java/com/example/cscb07s22finalproject/Venue.java
@@ -0,0 +1,18 @@
+package com.example.cscb07s22finalproject;
+
+import java.util.ArrayList;
+
+public class Venue {
+
+    String name;
+    ArrayList<String> sports;
+    ArrayList<Integer> codes;
+
+    public Venue(String name, ArrayList<String> sports, ArrayList<Integer> codes){
+
+        this.name = name;
+        this.sports = sports;
+        this.codes = codes;
+
+    }
+}
