5f1f890e8eba860254a74ffb58bdbabef2ee8528
keerthiha-b <105596437+keerthiha-b@users.noreply.github.com>
2022-08-01 21:40:49 -0400

    Worked on design of user/signup page

--- app/src/main/java/com/example/cscb07s22finalproject/DataBase.java
+++ app/src/main/java/com/example/cscb07s22finalproject/DataBase.java
@@ -1,7 +1,12 @@
 package com.example.cscb07s22finalproject;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -14,6 +19,7 @@ public final class DataBase {
 
     public static final int INCORRECT_FORMAT = -1;
     public static final int DOES_NOT_EXIST = -2;
+    public static final int ALREADY_EXISTS = -4;
     public static final int INCORRECT_PASSWORD = -3;
     public static final int CAN_LOGIN = 0;
 
@@ -32,29 +38,28 @@ public final class DataBase {
         else user = new Customer(username, password);
     }
 
+
     /*
         TODO: public int checkUser(String username, String password)
         check if username & password are the right format: return -1 if not
-
         check if user exists: return -2 if user doesn't exist
                 prompt signup
-
         if user exists: check if password is right: return -3 if password is incorrect
                 prompt incorrect Password
-
         if user exists and password is right: return 0
      */
     public int checkUser(String username, String password) {
         Pattern pattern = Pattern.compile("\\w+");
         Matcher matcher_user = pattern.matcher(username);
         Matcher matcher_pass = pattern.matcher(password);
-
         if(!(matcher_user.matches()) || !(matcher_pass.matches()))
             return INCORRECT_FORMAT;   // incorrect format
 
-        //TODO: other checks
 
-        return DOES_NOT_EXIST;
+        //TODO: other checks
+//
+        return INCORRECT_PASSWORD;
+
     }
 
     public void createUser(String username, String password) {
