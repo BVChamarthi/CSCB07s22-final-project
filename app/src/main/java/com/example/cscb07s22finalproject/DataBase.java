8cdb5bec739c932ff5a61cb1df77bd679ca0c6c1
Laksh03 <67872211+Laksh03@users.noreply.github.com>
2022-08-08 14:28:56 -0400

    fixed a bug I found

--- app/src/main/java/com/example/cscb07s22finalproject/DataBase.java
+++ app/src/main/java/com/example/cscb07s22finalproject/DataBase.java
@@ -332,7 +332,9 @@ public final class DataBase {
 
     public void createVenue(String venueName, String[] activities){
         ref.child("Venues").child(venueName).child("venueName").setValue(venueName);
-        ref.child("Venues").child(venueName).child("sports").setValue(activities);
+        for(int i = 0; i < activities.length; i++){
+            ref.child("Venues").child(venueName).child("sports").child("sport" + (i+1)).setValue(activities[i]);
+        }
 
         venues.add(new Venue(venueName, new ArrayList<String>(Arrays.asList(activities))));
     }
